3790abae79250e5faba2f5f1cc348467139edc39
adam <adben97@gmail.com>
2021-01-24 21:32:11 -0500

    Cretaed basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const [songs, setSongs] = useState([
+    {
+      Title: "Wake up",
+      Artist: "Arcade Fire",
+      img_src: "./img/image.jpg",
+      src: "public/audio/What a beautiful Sunset! (radio mix) - Angelwing.wav"
+    },
+    {
+      Title: "Song2",
+      Artist: "Arcade Fire",
+      img_src: "./img/image.jpg",
+      src: "public/audio/What a beautiful Sunset! (radio mix) - Angelwing.wav"
+    },
+    {
+      Title: "Song3",
+      Artist: "Arcade Fire",
+      img_src: "./img/image.jpg",
+      src: "public/audio/What a beautiful Sunset! (radio mix) - Angelwing.wav"
+    },
+    {
+      Title: "Song5",
+      Artist: "Arcade Fire",
+      img_src: "./img/image.jpg",
+      src: "public/audio/What a beautiful Sunset! (radio mix) - Angelwing.wav"
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      COMPOMENETS HERE
     </div>
   );
 }
---
+++ src/components/player.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function player() {
+    return (
+        <div className="player">
+            <audio></audio>
+            <h4>Playing now</h4>
+            {/* Details */}
+            {/* {Controls} */}
+            <p><strong>Next Up: </strong>Song 2</p>
+        </div>
+    )
+}
+
+export default player
